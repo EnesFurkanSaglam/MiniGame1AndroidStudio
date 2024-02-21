83a5c6aceebad28f9238a163e1a138bdbd7805a4
Enes Furkan SAGLAM <enesfurkansaglam@gmail.com>
2024-02-21 13:10:35 +0300

    Part1 finished

---
+++ app/src/main/java/com/efs/minigame/ActivityNormalOyun.java
@@ -0,0 +1,36 @@
+package com.efs.minigame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityNormalOyun extends AppCompatActivity {
+
+    private String[] iller = {"Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya",
+            "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Balıkesir",
+            "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis",
+            "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum",
+            "Denizli", "Diyarbakır", "Düzce", "Elazığ", "Edirne", "Erzincan",
+            "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari",
+            "Hatay", "Iğdır", "Isparta", "Istanbul", "Izmir", "Kahramanmaraş",
+            "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kırıkkale",
+            "Kırklareli", "Kırşehir", "Kilis", "Kocaeli", "Konya", "Kütahya",
+            "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş",
+            "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya",
+            "Samsun", "Siirt", "Sinop", "Sivas", "Şanlıurfa", "Şırnak",
+            "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Uşak", "Van",
+            "Yalova", "Yozgat", "Zonguldak"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_oyun);
+    }
+
+    public void btnHarfAl(){
+
+    }
+    public void  btnTahminEt(){
+
+    }
+}
